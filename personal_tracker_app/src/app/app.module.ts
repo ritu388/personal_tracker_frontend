02fe72189030b9240524ae5298bc2d9ec6c4f092
Ritu Pandey <ritupandey@zentechinfo.com>
2025-06-18 17:48:25 +0530

    `Added ngx-spinner and toastr services for loading and error handling across the application`

--- personal_tracker_app/src/app/app.module.ts
+++ personal_tracker_app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './common/register/register.component';
@@ -18,6 +18,7 @@ import { ToastrModule } from 'ngx-toastr';
     LoginComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
--- personal_tracker_app/src/app/common/login/login.component.ts
+++ personal_tracker_app/src/app/common/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 import { LoginService } from 'src/app/service/auth/login.service';
 
 @Component({
@@ -12,7 +14,9 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   constructor(
     private service: LoginService,
-    private router: Router
+    private router: Router,
+    private spinner: NgxSpinnerService,
+    private toster: ToastrService
   ) { }
 
   ngOnInit(): void {
@@ -23,11 +27,11 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
+    this.spinner.show();
     let obj = {
       loginId: this.loginForm.value.loginId,
       password: this.loginForm.value.password
     }
-
     if (this.loginForm.valid) {
       this.service.login(obj).subscribe((res: any) => {
         if (res) {
@@ -36,9 +40,17 @@ export class LoginComponent implements OnInit {
           localStorage.setItem('full_name', res.user.full_name);
           localStorage.setItem('email', res.user.email);
           localStorage.setItem('mobile_no', res.user.mobile_no);
+          this.spinner.hide();
           this.router.navigate(['home/Dashboard']);
+          this.toster.success('login successfully');
+        } else {
+          this.spinner.hide();
+          this.toster.error('error while login please check your password and loginId');
         }
       });
+    } else {
+      this.spinner.hide();
+      this.toster.info('please fill required field');
     }
   }
 
